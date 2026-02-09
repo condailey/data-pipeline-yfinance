a5fd390dd9750e8e1b3fad55df5ccb1b45240194
Connor Dailey <condailey1@gmail.com>
2026-02-09 15:33:03 -0600

    Add S3 upload to extract pipeline

--- extract.py
+++ extract.py
@@ -1,7 +1,11 @@
 import yfinance as yf
 import pandas as pd
+import boto3
 
+# List of tickers to fetch data for
 tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA']
+
+s3 = boto3.client('s3')
 
 # Fetch historical data for each ticker
 for ticker in tickers:
@@ -10,3 +14,5 @@ for ticker in tickers:
     cleaned_data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
     rounded_data = cleaned_data.round(3) 
     rounded_data.to_csv(f'data/{ticker}_raw_data.csv')
+    s3.upload_file(f'data/{ticker}_raw_data.csv', 'condailey-stock-data', f'{ticker}_raw_data.csv')
+
